4df94f5c48ee0a405b4f6295646a3cbb2bf0d968
Kevin Wang <kwangsan@gmail.com>
2020-03-22 21:45:17 -0400

    Getting started (#2)
    * add diesel and dotenv to Cargo.toml
    
    * check in Cargo.lock
    
    * gitignore .env
    
    * dbstring in .env; run `diesel setup`
    
    * run `diesel migration generate create_posts`
    
    * add sql migrations
    
    * run `diesel migration run`
    - this generates schema.rs
    
    * add comment in migration
    
    * add lib.rs
    
    * creates models.rs MODULE
    
    * add models & schema to lib.rs
    
    * add helper comments for myself
    
    * add question in lib.rs
    
    * add src/bin/show_posts.rs
    - leave helper comments

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+// if you hover over `models`, it allows you
+// to cmd+click into `src/models.rs`
+// - How?
+pub mod models;
+/**
+ * ... in schema.rs
+ *
+ * The table! macro creates a bunch of code
+ * based on the datbase schema to represent
+ * all of the tables and columns.
+ *
+ * Any time we run or revert a migration,
+ * this file will get automatically updated
+ */
+pub mod schema;
+
+pub fn establish_connection() -> PgConnection {
+	dotenv().ok();
+
+	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+	PgConnection::establish(&database_url).expect(&format!("Error connection to {}", database_url))
+}
