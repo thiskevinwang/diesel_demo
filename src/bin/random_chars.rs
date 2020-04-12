4e2453457fa2767aa8bb9d3c6d52a9bcef37c356
Kevin Wang <kwangsan@gmail.com>
2020-04-12 08:08:56 -0400

    rework random_chars

--- src/bin/random_chars.rs
+++ src/bin/random_chars.rs
@@ -1,35 +1,42 @@
 use rand::distributions::Alphanumeric;
 use rand::prelude::*;
+use std::io::stdin;
 use std::iter;
-use std::io::{stdin};
+use std::time::Instant;
+
+fn generate_password_from_length(length: usize) -> String {
+    let mut rng = thread_rng();
+    iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .take(length)
+        .collect()
+}
 
 /// cargo run --bin random_chars
 fn main() {
     println!("Specify length of password...");
-    
     // Read integer from user input
     // https://stackoverflow.com/a/30355925/9823455
     let mut input_value = String::new();
-    stdin().read_line(&mut input_value).expect("failed to read from stdin");
+    stdin()
+        .read_line(&mut input_value)
+        .expect("failed to read from stdin");
     let input_value = &input_value;
 
     // trim input
     // parse it
     let test = input_value.trim().parse::<u32>();
-    
     // return if test is not an integer
     match test {
         Ok(ok) => println!("You've specified: {}\n", ok),
-        Err(e) => return println!("Error: ({})", e), 
+        Err(e) => return println!("Error: ({})", e),
     };
 
     // unwrap the Result
     let test = test.unwrap();
 
-    let mut rng = thread_rng();
-    let chars: String = iter::repeat(())
-        .map(|()| rng.sample(Alphanumeric))
-        .take(test as usize) // cast `u32` to `usize`
-        .collect();
-    println!("{}", chars)
+    let start = Instant::now();
+    let chars = generate_password_from_length(test as usize); // cast `u32` to `usize`
+    let duration = start.elapsed();
+    println!("{} | generated in {:?}", chars, duration)
 }
