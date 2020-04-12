e036235ff340b5033d9c201521d67b339c6a27ba
Kevin Wang <kwangsan@gmail.com>
2020-04-12 13:49:05 -0400

    update get-user fn's

--- src/bin/get_user_1.rs
+++ src/bin/get_user_1.rs
@@ -11,11 +11,30 @@ use self::rust_server::*;
 // This brings `User` into scope
 use self::models::*;
 
-/// cargo run --bin get_user_1
-fn main() {
+pub fn get_user_by_id(primary_key: i32) -> User {
     use rust_server::schema::Users::dsl::*;
 
     let connection = establish_connection();
+    let user = Users
+        .find(primary_key)
+        .get_result::<User>(&connection)
+        .unwrap();
+
+    return user;
+}
+
+/// # get_user_1
+/// This function gets the User with `id: 1`.
+///
+/// Panics if that user doesn't exist
+///
+/// ```bash
+/// cargo run --bin get_user_1
+/// ```
+fn main() {
+    // use rust_server::schema::Users::dsl::*;
+
+    // let connection = establish_connection();
 
     // note:
     // User is the model
@@ -23,6 +42,8 @@ fn main() {
 
     // add `.unwrap()` at the end to fix error
     // > no field `first_name` on type `std::result::Result<rust_server::models::User, diesel::result::Error>`
-    let user = Users.find(1).get_result::<User>(&connection).unwrap();
-    println!("{:#?}", user);
+    // let user = Users.find(1).get_result::<User>(&connection).unwrap();
+
+    let user = get_user_by_id(1);
+    println!("{:?}", user);
 }
